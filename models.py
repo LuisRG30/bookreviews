c30017e63c00c2645135d99e41b67e50f5f65d5d
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-06-27 16:27:24 -0500

    created database models

---
+++ models.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, nullable = False)
+    password = db.Column(db.String, nullable = False)
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer, primary_key = True)
+    isbn = db.Column(db.String, nullable = False)
+    title = db.Column(db.String, nullable = False)
+    author = db.Column(db.String, nullable = False)
+    year = db.Column(db.Integer, nullable = False)
+
+    def add_review(self, score, review):
+        r = Review(score = score, review = review, book_id = self.id)
+        db.session.add(r)
+        db.session.commit()
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+    id = db.Column(db.Integer, primary_key = True)
+    score = db.Column(db.Integer, nullable = False)
+    review = db.Column(db.String, nullable = False)
+    book_id = db.Column(db.Integer, db.ForeignKey("books.id"), nullable = False)
