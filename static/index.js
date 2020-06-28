784020056a5b373ef65a4195692956acbf3cf809
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-06-28 10:41:49 -0500

    added js file for signup page

---
+++ static/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector("#signup").onsubmit = () => {
+        const password = document.querySelector("#pass").value;
+        const confirmation = document.querySelector("#confirm").value;
+
+        if (password === confirmation){
+
+        }
+        else{
+            
+        }
+    }
+})
